8e85d078072800a6a186c18d5549e45c44ffe997
Leonardo Vizagre <leonardo.vizagre@gmail.com>
2023-10-31 23:16:56 -0300

    Feat: add code style

--- src/server/Server.ts
+++ src/server/Server.ts
@@ -2,8 +2,9 @@ import express from 'express';
 
 const server = express(); 
 
+
 server.get('/', (req, res) => {    
-    return res.send ('Olá Dev!')
+  return res.send ('Olá Dev!');
 });
 
 export { server };
